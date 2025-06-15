5c657f98f5872c8fe4d015afe007a81001a940e8
Abbas0vv <ilhamabbasov46@gmail.com>
2025-06-15 17:34:20 +0400

    N-Tier Architecture Structure Completed

--- Quickeat/Program.cs
+++ Quickeat/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Identity;
+
 namespace Quickeat
 {
     public class Program
@@ -5,9 +7,44 @@ namespace Quickeat
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            var connectionString = builder.Configuration.GetConnectionString("Default");
+            builder.Services.AddDbContext<JobwayDbContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<JobwayDbContext>();
+
+            builder.Services.AddControllersWithViews()
+                            .AddRazorRuntimeCompilation();
+            builder.Services.AddRazorPages();
+
+            builder.Services.Configure<IdentityOptions>(options =>
+            {
+                // Password settings.
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredLength = 6;
+
+                // Lockout settings.
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                options.Lockout.MaxFailedAccessAttempts = 5;
+                options.Lockout.AllowedForNewUsers = true;
+
+                // User settings.
+                options.User.AllowedUserNameCharacters =
+                "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789._";
+                options.User.RequireUniqueEmail = true;
+            });
+
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            app.UseStaticFiles();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.Run();
         }
